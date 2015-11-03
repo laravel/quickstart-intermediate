51479d7f15c22ce236c08882082e9f7816d0fbe1
Dawlatzai <dawlatzai@netlinks.ws>
2015-11-03 14:24:26 -0800

    Add edit route in routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 // Task Routes
 Route::get('/tasks', 'TaskController@index');
 Route::post('/task', 'TaskController@store');
+Route::get('/task/{task}/edit', 'TaskController@edit');
 Route::delete('/task/{task}', 'TaskController@destroy');
 
 // Authentication Routes...
