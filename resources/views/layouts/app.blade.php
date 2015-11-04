989a1fcbda0ad072958da9e79d4ed7f285f5dcb7
James Brooks <jbrooksuk@me.com>
2015-11-04 22:25:33 +0000

    Navbar can link to index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,7 @@
 						<span class="icon-bar"></span>
 					</button>
 
-					<a class="navbar-brand" href="#">Task List</a>
+					<a class="navbar-brand" href="/">Task List</a>
 				</div>
 
 				<div id="navbar" class="navbar-collapse collapse">
