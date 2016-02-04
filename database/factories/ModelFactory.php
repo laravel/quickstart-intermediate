77a2cf92df9d46be06557a53d7266efd204703f6
Peter Michaux <petermichaux@gmail.com>
2016-02-04 07:26:15 -0800

    add automated tests

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,11 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+
+$factory->define(App\Task::class, function (Faker\Generator $faker) {
+    return [
+        'name' => str_random(10),
+    ];
+});
+
