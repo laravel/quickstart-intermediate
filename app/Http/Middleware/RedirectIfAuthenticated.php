5cfad6cd65c4e31e85c2269f15df2efe475ca79e
Werewolflsp <werewolflsp@gmail.com>
2016-04-10 18:05:59 +0800

    redirect to edInfos

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -35,7 +35,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next)
     {
         if ($this->auth->check()) {
-            return redirect('/tasks');
+            return redirect('/edInfos');
         }
 
         return $next($request);
