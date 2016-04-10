d8c1a7dc5beb34ad8e99a1877197f9e68e0837bc
Werewolflsp <werewolflsp@gmail.com>
2016-04-10 17:54:13 +0800

    change Header in navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,7 @@
 						<span class="icon-bar"></span>
 					</button>
 
-					<a class="navbar-brand" href="/">A Place Holder</a>
+					<a class="navbar-brand" href="/">Ed Info System</a>
 				</div>
 
 				<div id="navbar" class="navbar-collapse collapse">
