0f727661f7d06863510f2fc7b9b9e4575bf309e2
Werewolflsp <werewolflsp@gmail.com>
2016-04-10 23:51:23 +0800

    added isAdmin for user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password', 60);
+            $table->boolean('isAdmin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
