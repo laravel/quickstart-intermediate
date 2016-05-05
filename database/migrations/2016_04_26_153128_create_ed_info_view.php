74e7ec9aef2bb3c219aaf461da5ce7d7abf699ee
Werewolflsp <werewolflsp@gmail.com>
2016-05-06 01:48:58 +0800

    create two views to be compatible with mysql in redhat

--- database/migrations/2016_04_26_153128_create_ed_info_view.php
+++ database/migrations/2016_04_26_153128_create_ed_info_view.php
@@ -12,10 +12,11 @@ class CreateEdInfoView extends Migration
      */
     public function up()
     {
+        DB::statement('CREATE VIEW latest_ed_time AS
+                        SELECT ed_id as latest_ed, MAX(test_time) as latest_time FROM ed_infos GROUP BY ed_id');
         DB::statement('CREATE VIEW latest_ed_infos AS
                       SELECT ed_infos.* 
-                      FROM ed_infos RIGHT JOIN 
-                      (SELECT ed_id as latest_ed, MAX(test_time) as latest_time FROM ed_infos GROUP BY ed_id) as latest 
+                      FROM ed_infos RIGHT JOIN latest_ed_time as latest 
                       ON ed_id = latest_ed AND test_time = latest_time');
     }
 
@@ -26,6 +27,7 @@ class CreateEdInfoView extends Migration
      */
     public function down()
     {
+        DB::statement('DROP VIEW latest_ed_time');
         DB::statement('DROP VIEW latest_ed_infos');
     }
 }
